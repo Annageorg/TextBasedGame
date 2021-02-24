5a75be53a26e54f50e0a21e04106fb71a206bf29
Anna Georgiadou <77627644+Annageorg@users.noreply.github.com>
2021-02-24 14:07:48 +0000

    style made start of js

---
+++ neilExample/modules/item.js
@@ -0,0 +1,36 @@
+class Item {
+    constructor(name) {
+      this._name = name;
+      this._description = "";
+    }
+  
+    set name(value) {
+      if (value.length < 4) {
+        alert("Name is too short.");
+        return;
+      }
+      this._name = value;
+    }
+  
+    set description(value) {
+      if (value.length < 4) {
+        alert("Decription is too short.");
+        return;
+      }
+      this._name = value;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get description() {
+      return this._description;
+    }
+  
+    describe() {
+      return "The " + this._name + " is " + this._description;
+    }
+  
+  
+}
